5069683bf7d1438748a6fb1e973f49ef5ca8269d
CDVCvask <143233430+CDVCvask@users.noreply.github.com>
2025-08-21 17:08:28 -0600

    Clases3

--- Tienda_verde.py
+++ Tienda_verde.py
@@ -37,3 +37,33 @@ class Suppliers:
         self.Adress = Adress
         self.Mail = Mail
         self.Category = Category
+class Sells:
+    def __init__(self,ID_Sell,Date,Client,Employee,Total):
+        self.ID_Sell = ID_Sell
+        self.Date = Date
+        self.Client = Client
+        self.Employee = Employee
+        self.Total = Total
+class Sells_Details:
+    def __init__(self,ID_SD,Quantity,Product,Price,SubTotal):
+        self.ID_SD = ID_SD
+        self.Quantity = Quantity
+        self.Product = Product
+        self.Price = Price
+        self.SubTotal = SubTotal
+class Purchase:
+    def __init__(self,ID_Pur,Date,Supplier,Employee,Total):
+        self.ID_Pur = ID_Pur
+        self.Date = Date
+        self.Supplier = Supplier
+        self.Employee = Employee
+        self.Total = Total
+class Purchase_Details:
+    def __init__(self,ID_PD,Purchase,Quantity,Product,P_Price,SubTotal,Expiration):
+        self.ID_PD = ID_PD
+        self.Purchase = Purchase
+        self.Quantity = Quantity
+        self.Product = Product
+        self.P_Price = P_Price
+        self.SubTotal = SubTotal
+        self.Expiration = Expiration
